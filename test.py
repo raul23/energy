e1888937f4866b9cf4e71b8704d26b0648627871
Raul <rchfe23@gmail.com>
2021-01-19 23:33:42 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
-# Primality tests: trial division, Miller–Rabin, AKS
+# Primality tests: trial division, probability tests (e.g. Miller–Rabin), AKS
 # Prime Sieves
 # List of computer algebra systems: SageMath, SymPy
