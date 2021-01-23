1d018efd38a030b6101f130926ca6e757ebe2bb9
Raul <rchfe23@gmail.com>
2021-01-22 23:56:07 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,3 +2,17 @@
 # Prime Sieves
 # List of computer algebra systems: SageMath, SymPy
 # Ref.: https://en.wikipedia.org/wiki/Primality_test
+
+# Ref.: https://en.wikipedia.org/wiki/Primality_test#Python_Code
+def is_prime(n: int) -> bool:
+    """Primality test using 6k+-1 optimization."""
+    if n <= 3:
+        return n > 1
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i ** 2 <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
